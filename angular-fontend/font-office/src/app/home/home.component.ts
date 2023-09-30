cf3d3e7ba966a981b6a8540081e085e4d2c951a9
guymn <117151853+guymn@users.noreply.github.com>
2023-09-30 23:50:50 +0700

    footer

--- angular-fontend/font-office/src/app/home/home.component.ts
+++ angular-fontend/font-office/src/app/home/home.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  private carousel: number = 1;
 
+  getCarousel(con: any): boolean {
+    return this.carousel == con;
+  }
+  next() {
+    this.carousel++;
+  }
+
+  previous() {
+    this.carousel--;
+  }
 }
