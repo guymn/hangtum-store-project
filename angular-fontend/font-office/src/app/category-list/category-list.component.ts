0e756f356a9c4cb887e4e6eab134c3f33d945206
guymn <117151853+guymn@users.noreply.github.com>
2023-10-01 02:44:26 +0700

    seroll category list

--- angular-fontend/font-office/src/app/category-list/category-list.component.ts
+++ angular-fontend/font-office/src/app/category-list/category-list.component.ts
@@ -6,27 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent {
-  category: string[] = [];
-  private carousel: number = 1;
+  private category: string[] = [];
+  private data: string[] = [];
+  private carouselFirst: number = 0;
+  private carouselLast: number = 5;
 
   constructor() {
     this.setCategory();
+    this.setData();
+  }
+
+  getCategoryToShow() {
+    return this.data;
+  }
+
+  setData() {
+    this.data = [];
+    let temp = this.carouselFirst;
+    for (let i = 0; i < 5; i++) {
+      this.data.push(this.category[temp]);
+      temp = (temp + 1) % this.getCategorySize();
+    }
+  }
+
+  getCategorySize() {
+    return this.category.length;
   }
 
   setCategory() {
     for (let i = 0; i < 30; i++) {
-      this.category.push('ยาฆ่าหญ้า');
+      this.category.push('ยาฆ่าหญ้า' + i);
     }
   }
 
-  getCarousel(con: any): boolean {
-    return this.carousel == con;
-  }
   next() {
-    this.carousel++;
+    this.carouselFirst = (this.carouselFirst + 1) % this.getCategorySize();
+    this.carouselLast = (this.carouselLast + 1) % this.getCategorySize();
+    this.setData();
   }
 
   previous() {
-    this.carousel--;
+    if (this.carouselFirst == 0) {
+      this.carouselFirst = this.getCategorySize();
+    }
+    if (this.carouselLast == 0) {
+      this.carouselLast = this.getCategorySize();
+    }
+    this.carouselFirst--;
+    this.carouselLast--;
+    this.setData();
   }
 }
