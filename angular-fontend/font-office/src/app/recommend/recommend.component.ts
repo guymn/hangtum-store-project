5f5a51fbd6c68565b75fdb2df76505516f8b0418
guymn <117151853+guymn@users.noreply.github.com>
2023-10-05 19:25:18 +0700

    recommend

--- angular-fontend/font-office/src/app/recommend/recommend.component.ts
+++ angular-fontend/font-office/src/app/recommend/recommend.component.ts
@@ -7,15 +7,37 @@ import { Component } from '@angular/core';
 })
 export class RecommendComponent {
   private carousel: number = 1;
+  private pathImage: string[] = [];
 
-  getCarousel(con: any): boolean {
+  constructor() {
+    for (let i = 1; i < 4; i++) {
+      this.pathImage.push(`../../assets/img/recommend/${i}.jpg`);
+    }
+  }
+
+  getPathImage(num: number): string {
+    return this.pathImage[num - 1];
+  }
+
+  getCarousel(): number {
+    return this.carousel;
+  }
+
+  checkCarousel(con: number): boolean {
     return this.carousel == con;
   }
+
   next() {
+    if (this.carousel == this.pathImage.length) {
+      this.carousel = 0;
+    }
     this.carousel++;
   }
 
   previous() {
+    if (this.carousel == 1) {
+      this.carousel = this.pathImage.length+1;
+    }
     this.carousel--;
   }
 }
