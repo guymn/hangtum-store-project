3c981e262514bfb4437f610ad930e300c8d66f40
guymn <117151853+guymn@users.noreply.github.com>
2023-10-05 19:54:11 +0700

    auto stroll img

--- angular-fontend/font-office/src/app/recommend/recommend.component.ts
+++ angular-fontend/font-office/src/app/recommend/recommend.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recommend',
   templateUrl: './recommend.component.html',
   styleUrls: ['./recommend.component.css'],
 })
-export class RecommendComponent {
+export class RecommendComponent implements OnInit{
   private carousel: number = 1;
   private pathImage: string[] = [];
+  private intervalId: any; // This will store the interval ID
+  private intervalDuration: number = 2500; // Set the interval duration in milliseconds
 
   constructor() {
     for (let i = 1; i < 4; i++) {
       this.pathImage.push(`../../assets/img/recommend/${i}.jpg`);
+    }
+  }
+
+  ngOnInit() {
+    this.startInterval();
+  }
+
+  ngOnDestroy() {
+    // Don't forget to clear the interval when the component is destroyed
+    this.clearInterval();
+  }
+
+  startInterval() {
+    // Clear any existing interval and start a new one
+    this.clearInterval();
+    this.intervalId = setInterval(() => {
+      this.next();
+    }, this.intervalDuration);
+  }
+
+  clearInterval() {
+    // Clear the interval if it exists
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
     }
   }
 
@@ -32,12 +58,15 @@ export class RecommendComponent {
       this.carousel = 0;
     }
     this.carousel++;
+    // Reset the interval timer to 2 seconds when manually triggered
+    this.startInterval();
   }
 
   previous() {
     if (this.carousel == 1) {
-      this.carousel = this.pathImage.length+1;
+      this.carousel = this.pathImage.length + 1;
     }
     this.carousel--;
+    this.startInterval();
   }
 }
