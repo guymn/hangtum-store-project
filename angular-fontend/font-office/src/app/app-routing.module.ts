93ec3433870360d0d0231229f905e7c56f93a6cb
guymn <117151853+guymn@users.noreply.github.com>
2023-10-05 21:35:30 +0700

    router

--- angular-fontend/font-office/src/app/app-routing.module.ts
+++ angular-fontend/font-office/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { ProductInCategoryComponent } from './product-in-category/product-in-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CategoryListComponent },
+  { path: 'product', component: ProductInCategoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
